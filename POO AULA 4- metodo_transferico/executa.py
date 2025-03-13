bff9a0e07dcc73abcb209f683c2d667376a57831
FranBR76 <francescovenancio@gmail.com>
2025-03-13 20:32:50 -0300

    new file:   POO AULA 4- metodo_transferico/__pycache__/conta.cpython-312.pyc 	new file:   POO AULA 4- metodo_transferico/__pycache__/pessoa.cpython-312.pyc 	modified:   POO AULA 4- metodo_transferico/executa.py

--- POO AULA 4- metodo_transferico/executa.py
+++ POO AULA 4- metodo_transferico/executa.py
@@ -1,10 +1,17 @@
 from conta import Conta
 
 c1 = Conta('123-4', 'Rodrigo', 120.0, 1000.0)
-c2 = Conta('234-5', 'Gilson', 50.0, 1000.0)
-# c1 é um atributo de referencia para um objeto para que isso ocorra no python sao acionados de forma transparente os metodos magicos 
-# __new__() e __init__() responsaveis por construir e iniciar objetos.
+c2 = Conta('234-5', 'Gilson', 10.0, 1000.0)
 c3 = Conta('123-4', 'Rodrigo', 120.0, 1000.0)
+
+print(c2.saldo)
+# c1.depositar(100.0)
+print(c1.saldo)
+c2.depositar(300.0)
+print(c2.saldo)
+c2.transferencia(c1,30)
+print(c1.saldo)
+print(c2.saldo)
 
 
 # print(c2.saldo)
@@ -15,10 +22,10 @@ c3 = Conta('123-4', 'Rodrigo', 120.0, 1000.0)
 # c1.depositar(10000.0)
 # print(c1.saldo)
 
-if(c1 == c3):
-    print("Contas são iguais")
-else:
-    print("Contas são diferentes")
+# if(c1 == c3):
+#     print("Contas são iguais")
+# else:
+#     print("Contas são diferentes")
 
 
 from pessoa import Pessoa
@@ -28,18 +35,18 @@ p2 = Pessoa('Cesco Matteo', 19, 905.10)
 p3 = Pessoa('Mateus Ortileb', 18, 900.0)
 p4 = Pessoa('Pedro Dias', 21, 1000.0)
 
-p1.exibeNome()
-p1.exibeIdade()
-p1.exibeSalario()
+# p1.exibeNome()
+# p1.exibeIdade()
+# p1.exibeSalario()
 
-p2.exibeNome()
-p2.exibeIdade()
-p2.exibeSalario()
+# p2.exibeNome()
+# p2.exibeIdade()
+# p2.exibeSalario()
 
-p3.exibeNome()
-p3.exibeIdade()
-p3.exibeSalario()   
+# p3.exibeNome()
+# p3.exibeIdade()
+# p3.exibeSalario()   
 
-p4.exibeNome()
-p4.exibeIdade()
-p4.exibeSalario()
+# p4.exibeNome()
+# p4.exibeIdade()
+# p4.exibeSalario()
