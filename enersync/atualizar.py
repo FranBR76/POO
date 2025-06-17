4637b97893a3a2a421302f4b106c74021d33e98e
FranBR76 <francescovenancio@gmail.com>
2025-06-16 22:14:10 -0300

    modified:   enersync/__pycache__/app.cpython-312.pyc 	modified:   enersync/__pycache__/atualizar.cpython-312.pyc 	modified:   enersync/__pycache__/login.cpython-312.pyc 	modified:   enersync/__pycache__/metodos.cpython-312.pyc 	modified:   enersync/__pycache__/planoViagem.cpython-312.pyc 	modified:   enersync/__pycache__/registrar.cpython-312.pyc 	modified:   enersync/__pycache__/usuario.cpython-312.pyc 	modified:   enersync/__pycache__/usuarioDAO.cpython-312.pyc 	modified:   enersync/__pycache__/veiculo.cpython-312.pyc 	modified:   enersync/__pycache__/veiculoDAO.cpython-312.pyc 	modified:   enersync/__pycache__/veiculo_app.cpython-312.pyc 	modified:   enersync/atualizar.py

--- enersync/atualizar.py
+++ enersync/atualizar.py
@@ -16,39 +16,53 @@ class AtualizarDados(Voltar):
         self.root.geometry("1280x720")
         self.root.configure(bg="lightblue")
 
-        tk.Button(root, text="< Voltar", command=self.voltar, bg="#333", fg="White").pack(pady=20, anchor="w")
+        
 
-        self.label_nome = tk.Label(root, text="Nome:", font="Arial: 15")
+        tk.Button(root, text="< Voltar", command=self.voltar, font="Arial: 20", bg="#333", fg="White").pack(pady=20,padx="20", anchor="w")
+
+        self.card = tk.Frame(root, bg="#e4e4e4", bd=2, relief="raised", padx=10, pady=10)
+        self.card.pack(padx=20, pady=20, fill="both", expand=True)
+
+        # Container horizontal: esquerda = formulário | direita = imagem
+        self.container = tk.Frame(self.card, bg="#f2f2f2")
+        self.container.pack(fill="both", expand=True)
+
+        # Frame para os campos de entrada (lado esquerdo)
+        self.form_frame = tk.Frame(self.container, bg="white")
+        self.form_frame.pack(side="left", fill="both", expand=True, padx=10, pady=10)
+
+
+        self.label_nome = tk.Label(self.form_frame, text="Nome:", bg="white",font="Arial: 30")
         self.label_nome.pack()
-        self.entry_nome = tk.Entry(root, font="Arial: 15")
+        self.entry_nome = tk.Entry(self.form_frame, bg="#f2f2f2", font="Arial: 30")
         self.entry_nome.pack()
 
-        self.label_email = tk.Label(root, text="Email:", font="Arial: 15")
+        self.label_email = tk.Label(self.form_frame, bg="white", text="Email:", font="Arial: 30")
         self.label_email.pack()
-        self.entry_email = tk.Entry(root, font="Arial: 15")
+        self.entry_email = tk.Entry(self.form_frame, bg="#f2f2f2",  font="Arial: 30")
         self.entry_email.pack()
 
-        self.label_senha = tk.Label(root, text="Senha:", font="Arial: 15")
+        self.label_senha = tk.Label(self.form_frame, bg="white", text="Senha:", font="Arial: 30")
         self.label_senha.pack()
-        self.entry_senha = tk.Entry(root, show="*", font="Arial: 15")
+        self.entry_senha = tk.Entry(self.form_frame, bg="#f2f2f2",  show="*", font="Arial: 30")
         self.entry_senha.pack()
 
-        # self.label_cpf = tk.Label(root, text="CPF:", font="Arial: 15")
+        # self.label_cpf = tk.Label(self.form_frame, text="CPF:", font="Arial: 30")
         # self.label_cpf.pack()
-        # self.entry_cpf = tk.Entry(root, font="Arial: 15")
+        # self.entry_cpf = tk.Entry(self.form_frame, bg="#f2f2f2", font="Arial: 30")
         # self.entry_cpf.pack()
 
-        self.label_dt_nasc = tk.Label(root, text="Data de Nascimento (YYYY-MM-DD):", font="Arial: 18")
+        self.label_dt_nasc = tk.Label(self.form_frame, bg="white", text="Data de Nascimento (YYYY-MM-DD):", font="Arial: 30")
         self.label_dt_nasc.pack()
-        self.entry_dt_nasc = tk.Entry(root, font="Arial: 15")
+        self.entry_dt_nasc = tk.Entry(self.form_frame, bg="#f2f2f2", font="Arial: 30")
         self.entry_dt_nasc.pack()
 
-        # self.label_id = tk.Label(root, text="ID (Para atualizar/deletar):", font="Arial: 15")
+        # self.label_id = tk.Label(root, text="ID (Para atualizar/deletar):", font="Arial: 30")
         # self.label_id.pack()
-        # self.entry_id = tk.Entry(root, font="Arial: 15")
+        # self.entry_id = tk.Entry(root, font="Aria bg="#f2f2f2",l: 30")
         # self.entry_id.pack()
 
-        tk.Button(root, text="Atualizar", command=self.atualizar, font="Arial: 15", bg="#333", fg="white").pack(side="left", padx=30)
+        tk.Button(self.form_frame, text="Atualizar", command=self.atualizar, font="Arial: 30", bg="#333", fg="white").pack(side="left", padx=30)
 
 
     def atualizar(self):
